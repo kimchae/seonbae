4728037cc8b0bbdd6fe55f669269dba7815832da
Matthieu Tran <matthieuktran@gmail.com>
2019-05-24 14:25:49 -0700

    Add initial apollo-server and graphql setup

---
+++ server/index.js
@@ -0,0 +1,68 @@
+import { ApolloServer, gql, makeExecutableSchema } from 'apollo-server';
+
+import mongoose from 'mongoose';
+import { merge } from 'lodash';
+
+const root = gql`
+  type Query {
+    _empty: String
+  }
+
+  type Mutation {
+    _empty: String
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`;
+
+// For use with graphql-constraint-directive
+const formatError = function(err) {
+  if (err.message.startsWith('Expected type Constraint')) {
+    err.message = err.message.split('; ')[1];
+  }
+
+  return err;
+};
+
+const typeDefs = [root];
+const resolvers = merge(
+  {}
+  /**
+   * Query Resolvers
+   */
+
+  /**
+   * Mutation Resolvers
+   */
+
+  /**
+   * Subscriptions
+   */
+);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  schemaDirectives: {},
+  resolvers
+});
+
+const server = new ApolloServer({ schema, formatError });
+const port = process.env.PORT || 8080;
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/seonbae?retryWrites=true', {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB Service');
+
+    server.listen({ port }).then(({ url }) => {
+      console.log(`🚀  Server ready at ${url} 🛸`);
+    });
+  })
+  .catch(err => {
+    throw new Error(err);
+  });
